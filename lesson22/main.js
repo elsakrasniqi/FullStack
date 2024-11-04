a5cc34e790c58017b1e4784ca09e624f1a5af203
elsakrasniqi <eelsakrasniqi@gmail.com>
2024-11-04 12:10:14 +0100

    conditionals

---
+++ lesson22/main.js
@@ -0,0 +1,29 @@
+var x=10;
+var y=5;
+
+//10>5 -- yes it is
+// if(x>y){
+//     console.log("This is true");
+// }else{
+//     console.log("This is false");
+// }
+
+
+if(x<y){
+    console.log("This is false");
+}else{
+    console.log("This is true");
+}
+
+
+if(y>x){
+    console.log("This is true");
+}else{
+    console.log("This is false");
+}
+
+if(x==7){
+    console.log("10 equals 7");
+}else{
+    console.log("10 does not equal 7");
+}
