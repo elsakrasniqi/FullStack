40c073ff9b7e652a26b2a57ac79ecc467533bf12
elsakrasniqi <eelsakrasniqi@gmail.com>
2024-11-08 10:49:22 +0100

    ,

--- lesson25/main.js
+++ lesson25/main.js
@@ -1,6 +1,30 @@
+//detyra e pare
 var sum=0;
 
 for(x = 15; x<=47; x++){
     sum=sum+i;
-    console.log(sum);
 }
+console.log(sum);
+
+//detyra e dyte
+var array=[5,10,15,20,25];
+var total = 0;
+for (j=0; j<array.length;j++){
+    total+=array[j];
+}
+console.log(total);
+
+//menyre tjeter per detyren e dyte
+// for(j of array){  ose (j in array) = shfaq indexat jo vlerat
+//     total+=j; 
+// }
+// console.log(total);
+
+var emri = document.getElementbyId("name");
+var save = document.getElementById("btn_id");
+  var Lista = [];
+  save.addEventListener("click", function addElements(){
+    Lista.push(emri.value);
+    console.log(Lista); 
+  })
+  
