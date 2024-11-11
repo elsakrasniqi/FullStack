5c6fe6784366e30d1d4b096705242b4354268567
elsakrasniqi <eelsakrasniqi@gmail.com>
2024-11-11 12:30:35 +0100

    circle game

--- lesson28/main.js
+++ lesson28/main.js
@@ -7,18 +7,48 @@
 // }
 // printNames();
 
-var colors = ['red', 'green', 'blue', 'purple'];
-function changeColor(){
-    document.querySelector('body').style.background = 
-    colors[Math.floor(Math.random()*colors.length)];
-}
-changeColor();
+// var colors = ['red', 'green', 'blue', 'purple'];
+// function changeColor(){
+//     document.querySelector('body').style.background = 
+//     colors[Math.floor(Math.random()*colors.length)];
+// }
+// changeColor();
 
-var names = ['Antigona', 'Grenti', 'Donika'];
-function changeName(){
-    document.querySelector('p').innerHTML = 
-    names[Math.floor(Math.random()*names.length)];
+// var names = ['Antigona', 'Grenti', 'Donika'];
+// function changeName(){
+//     document.querySelector('p').innerHTML = 
+//     names[Math.floor(Math.random()*names.length)];
 
+// }
+// setInterval(changeColor, 1000);
+// setInterval(changeName, 2000);
+
+var circle = document.getElementById("circle");
+var showtime = document.getElementById("time");
+var timeStart = new Date().getTime();
+
+function showCircle(){
+    circle.style.display = "block";
+    circle.style.top = Math.random() * 500 + "px";
+    circle.style.left = Math.random() * 900 + "px";
+    circle.style.backgroundColor = randomColor();
+    timeStart = new Date().getTime();
 }
-setInterval(changeColor, 1000);
-setInterval(changeName, 2000);
+showCircle(); 
+circle.onclick = function(){
+    circle.style.display = "none";
+    setTimeout(showCircle, 1000);
+    var timeEnd = new Date().getTime();
+    var time=(timeEnd - timeStart)/1000;
+    showtime.innerHTML = time + 's';
+}
+
+function randomColor(){
+    var s = '0123456789ABCDEF';
+    var color = "#";
+
+    for(i=0; i<6; i++){
+        color = color + s[Math.floor(Math.random()*16)];
+    }
+    return color;
+}
