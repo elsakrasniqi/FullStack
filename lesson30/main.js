838daf65737efa1b4169ed8419d78aeb1e9cf46d
elsakrasniqi <eelsakrasniqi@gmail.com>
2024-11-13 11:51:14 +0100

    jQuery intro

---
+++ lesson30/main.js
@@ -0,0 +1,14 @@
+//js
+//document.getElementById("myElement").style.display = "none";
+
+//jQuery
+$('myElement').hide();
+// same as jQuery('myElement').hide();
+// $('#myElement').text("New Text"); //manipulate with html
+$('.btn').click(function*(){
+    //alert("clicked");
+    console.log($('#myElement').text());
+
+    $('#myElement').text('Testing text');
+    $('#myElement').append('<br> Extra text');
+});
