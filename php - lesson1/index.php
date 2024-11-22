8deef440da74e29501446200091d8937e3ad8f0d
elsakrasniqi <eelsakrasniqi@gmail.com>
2024-11-22 11:18:01 +0100

    intro

--- php - lesson1/index.php
+++ php - lesson1/index.php
@@ -1,3 +1,15 @@
-<?php
-echo "hello world";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>My first PHP Page</h1>
+    <?php
+    echo "Hello World";
+    ?>
+
+</body>
+</html>
